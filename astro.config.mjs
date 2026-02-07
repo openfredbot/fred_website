788a2639708f6a33ee7006d5a2c61e90fb311138
root <root@srv1341150.hstgr.cloud>
2026-02-07 17:15:59 +0000

    fix: update to Node 20 and configure astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap(),
+    icon(),
   ],
   output: 'static',
   build: {
